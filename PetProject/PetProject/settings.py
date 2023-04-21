2708da1379cbe67138d820340f1b03fe65106df9
Loic <loicpetitjeans+github@gmail.com>
2023-04-21 09:05:00 +0200

    Templates fixed

--- PetProject/PetProject/settings.py
+++ PetProject/PetProject/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'PetProject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -130,4 +130,6 @@ REST_FRAMEWORK = {
     'PAGE_SIZE': 10
 }
 
-TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
+TEMPLATE_DIRS = (
+    os.path.join(BASE_DIR, 'templates'),
+)
